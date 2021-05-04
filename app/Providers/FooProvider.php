09c6b95bf5801fdecfecc22f3c463b63769a2343
SnapnetDev <39124697+SnapnetDev@users.noreply.github.com>
2021-05-04 16:17:42 +0100

    np

---
+++ app/Providers/FooProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FooProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+       $this->publishes(__DIR__ . '/../Http/Controllers/FooController.php'=>app_path('Http/Controllers/FooController.php'));
+       $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+       
+    }
+
+}
